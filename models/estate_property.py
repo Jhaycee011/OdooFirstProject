bb7e503a8b4d99ed654ffdfd03de0528253a915f
Gerwin Ogot <gerwinogot@gmail.com>
2023-08-23 15:03:51 +0800

    New fields, added some tree and kanban view and added notebook pages on form view.

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,5 +1,6 @@
-from odoo import  models, fields
+from odoo import models, fields
 import datetime
+
 
 class EstateProperty (models.Model):
     _name = "estate.property"
@@ -12,7 +13,7 @@ class EstateProperty (models.Model):
     expected_price = fields.Float(string='Expected Price', required=True,align='left')
     selling_price = fields.Float(string='Selling Price',align='left',readonly=True,no_copy=True)
     bedrooms = fields.Integer(string='Bedrooms',align='left',default=2)
-    living_area = fields.Integer(string='Living Area',align='left')
+    living_area = fields.Integer(string='Living Area (sqm)',align='left')
     facades = fields.Integer(string='Facades',align='left')
     garage = fields.Boolean(string='Garage',align='left')
     garden = fields.Boolean(string='Garden',align='left')
@@ -23,3 +24,12 @@ class EstateProperty (models.Model):
     )
 
     active = fields.Boolean(string='Active', default=True)
+    active = fields.Boolean(string='Active', default=True)
+    x_pool = fields.Boolean(string='Pool')
+    x_sauna = fields.Boolean(string='Sauna')
+    x_gym = fields.Boolean(string='Gym')
+    x_laundry = fields.Boolean(string='Laundry services')
+
+# The default in expected_price was the on that didn't update on the database.
+# The variables that starts with "x_" are defined in Odoo localhost since it can't be modified/updated through this python code.
+# Will get an error about those variables not existing in estate.property model but it was inputted there already
