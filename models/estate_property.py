a3b5a2741358875a95b1b1b28ef36936a3348d29
Gerwin Ogot <gerwinogot@gmail.com>
2023-08-25 13:38:58 +0800

    Added some computed fields that will compute for the selling price

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 import datetime
 
 
@@ -10,8 +10,8 @@ class EstateProperty (models.Model):
     description = fields.Text(string='Description',align='left')
     postcode = fields.Char(string='Postcode',align='left')
     date_availability = fields.Date(string='Date Availability',align='left',no_copy=True, default=lambda self: (datetime.datetime.now() + datetime.timedelta(days=90)).strftime('%Y-%m-%d'))
-    expected_price = fields.Float(string='Expected Price', required=True,align='left')
-    selling_price = fields.Float(string='Selling Price',align='left',readonly=True,no_copy=True)
+    expected_price = fields.Float(string='Expected Price', required=True,align='left',default=2000000)
+    selling_price = fields.Float(string='Selling Price',align='left',readonly=True, compute='_compute_selling_price', store=True)
     bedrooms = fields.Integer(string='Bedrooms',align='left',default=2)
     living_area = fields.Integer(string='Living Area (sqm)',align='left')
     facades = fields.Integer(string='Facades',align='left')
@@ -22,14 +22,14 @@ class EstateProperty (models.Model):
         [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         string='Garden Orientation'
     )
-
-    active = fields.Boolean(string='Active', default=True)
     active = fields.Boolean(string='Active', default=True)
     x_pool = fields.Boolean(string='Pool')
     x_sauna = fields.Boolean(string='Sauna')
     x_gym = fields.Boolean(string='Gym')
     x_laundry = fields.Boolean(string='Laundry services')
+    restaurants = fields.Boolean(string='Restaurants')
 
-# The default in expected_price was the on that didn't update on the database.
-# The variables that starts with "x_" are defined in Odoo localhost since it can't be modified/updated through this python code.
-# Will get an error about those variables not existing in estate.property model but it was inputted there already
+    @api.depends('expected_price', 'living_area', 'bedrooms', 'garden', 'facades')
+    def _compute_selling_price(self):
+        for estate in self:
+            estate.selling_price = estate.expected_price * (estate.living_area + estate.bedrooms + estate.garden + estate.facades)
